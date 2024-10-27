9efbe186b1aa35bbcc92beebb241d1ce8b5ac4c3
SenaNur3 <se_na_e12@hotmail.com>
2024-10-27 21:50:45 +0300

    pages edited

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Button = ({text,url}) => {
   return (
     <Link href={url}>
-    <button className={styles.container}>
+    <button className={styles.button}>
       {text}
     </button>
     </Link>
