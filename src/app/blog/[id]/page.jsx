411b9f64b6d13294daca83ede02b4fe1c6255be4
SenaNur3 <se_na_e12@hotmail.com>
2024-10-31 16:50:58 +0300

    theme added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+
 const BlogPost = () => {
-  return <div>BlogPost</div>;
+  const [data] = useState({
+    title: "Blog Başlığı",
+    desc: "Bu, blog gönderisinin açıklamasıdır.",
+    img: "/hero.png", // Resim yolu
+    username: "Yazar Adı",
+    content: "Bu, blog gönderisinin içeriğidir. Uzun bir metin içerebilir."
+  });
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.desc}>{data.desc}</p>
+          <div className={styles.author}>
+            <Image
+              src={data.img}
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>{data.username}</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={data.img} alt="" fill={true} className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>{data.content}</p>
+      </div>
+    </div>
+  );
 };
 export default BlogPost;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
-
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
-
+import { ThemeProvider } from "../../context/ThemeContext";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,12 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
-        <div className="container">
-          <Navbar />
-          {children}
-          <Footer />
-        </div>
+      <body>
+        <ThemeProvider>
+          <div className="container">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
